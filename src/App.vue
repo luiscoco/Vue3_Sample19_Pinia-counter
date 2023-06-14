100f0296ccaf6102e4f114f407de7ebf54ecedcf
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-06-14 09:39:49 +0200

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <label>Counter: {{ counter.count }}</label>
+  </div>
+  <div>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+<script lang="ts" setup>
+import { useCounterStore } from "../src/store/store";
 
-export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-});
-</script>
+const counter = useCounterStore();
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+function increment() {
+  counter.increment();
 }
-</style>
+
+function decrement() {
+  counter.decrement();
+}
+</script>
